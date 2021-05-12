b14204f38de7dea958d11202cb52ae8d4a3759d9
dilpatel44 <64481184+dilpatel44@users.noreply.github.com>
2021-05-11 20:50:23 -0400

    Add files via upload

---
+++ Facemeshreact/src/App.js
@@ -0,0 +1,30 @@
+import React, {useRef} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import webcam from "react-webcam";
+function App() {
+
+  const webcamRef = useRef(null)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
